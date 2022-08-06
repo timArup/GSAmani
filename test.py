8f6401f8638ac058c515ff5c9b3869e55f3b8773
Tim Stuart <Tim.Stuart@arup.com>
2022-08-06 19:04:19 +0100

    Tidied up

--- test.py
+++ test.py
@@ -5,11 +5,9 @@ from gsapy import GSA
 from matplotlib import pyplot as plt
 # from pathlib import Path
 
-def create_list(prefix,first,last):
-    list = []
-    for i in range(first,last+1):
-        list.append(prefix + str(i))
-    return list
+def open_model():
+    model = GSA(r'.\\NPmodel_02.gwb', version = "10.1")
+    return model
 
 def user_params():
     global Cs_ULS
@@ -24,15 +22,14 @@ def user_params():
     global all_PN
     all_PN = create_list('PN',1,12)
 
-def open_model():
-    model = GSA(r'.\\NPmodel_02.gwb', version = "10.1")
-    return model
+    global addl_pts
+    addl_pts = 0
 
-def beam_results(no,combination):
-    return model.get_1D_elem_resultants(no, # the element number
-     combination, # analysis case or combination number
-    axis="local", # output axis - if omitted, it uses the default axis
-    addl_pts=0) # additional number of points along the element to output forces, if ommited it defaults to 0
+def create_list(prefix,first,last):
+    list = []
+    for i in range(first,last+1):
+        list.append(prefix + str(i))
+    return list
 
 def convert_to_2_list(to_string):
 
@@ -54,89 +51,67 @@ def convert_to_2_list(to_string):
     updated = [int(item) for item in updated]
     return updated
 
-def get_multiple_beam_results(elements):
-    # R0 = []
-    # R1 = []
-    # Rs = {}
-    # layers = {}
-    # # first = True
+def beam_results(no,combination,addl_pts):
+    return model.get_1D_elem_resultants(no, # the element number
+     combination, # analysis case or combination number
+    axis="local", # output axis - if omitted, it uses the default axis
+    addl_pts=addl_pts) # additional number of points along the element to output forces, if ommited it defaults to 0
 
+def distance_along_element(index,addl_pts):
+    return index/(addl_pts+1)
+
+def get_multiple_beam_results(elements):
     Fx = []
     Mres = []
     Cs = []
+    xDivL = []
+
     for combination in Cs_ULS:
+
         for element_no in elements:
-            results = beam_results(element_no,combination)
-        #     print(results)
-        #     R0.append({'0Fx':results[0][0],'0Fy':results[0][1],'0Fz':results[0][2],'0Mxx':results[0][3],'0Myy':results[0][4],'0Mzz':results[0][5]})
-        #     R1.append({'1Fx':results[1][0],'1Fy':results[1][1],'1Fz':results[1][2],'1Mxx':results[1][3],'1Myy':results[1][4],'1Mzz':results[1][5]})
+            results = beam_results(element_no,combination,addl_pts)
 
-        #     Rs.update({'0Fx':results[0][0],'0Fy':results[0][1],'0Fz':results[0][2],'0Mxx':results[0][3],'0Myy':results[0][4],'0Mzz':results[0][5],
-        # '1Fx':results[1][0],'1Fy':results[1][1],'1Fz':results[1][2],'1Mxx':results[1][3],'1Myy':results[1][4],'1Mzz':results[1][5]})
-        
-        #     layers.update({element_no: {'Res_0': {'Fx':results[0][0],'Fy':results[0][1],'Fz':results[0][2],'Mxx':results[0][3],'Myy':results[0][4],'Mzz':results[0][5]}, 'Res_1':
-        #     {'Fx':results[1][0],'Fy':results[1][1],'Fz':results[1][2],'Mxx':results[1][3],'Myy':results[1][4],'Mzz':results[1][5]}}})
-        #     print(Rs)  
+            for i in range(addl_pts + 1):
+                result = results[i]
 
-        # # return layers
-        # return Rs
-
-            Fx.append(results[0][0])
-            Fx.append(results[1][0])
-            Mres.append((results[0][4]**2+results[0][5]**2)**0.5)
-            Mres.append((results[1][4]**2+results[1][5]**2)**0.5)
-            Cs.append(combination)
+                Fx.append(result[0])
+                Fx.append(result[0])
+                Mres.append((result[4]**2+result[5]**2)**0.5)
+                Mres.append((result[4]**2+result[5]**2)**0.5)
+                Cs.append(combination)
+                xDivL.append(distance_along_element(i,addl_pts))
 
     return Fx, Mres, Cs
-
-def results_analysis(df):
-    df['WC_0'] = {'Fx': df['Res_0']['Fx'] ,'Mres': sqrt( df['Res_0']['Myy']**2 + df['Res_0']['Mzz']**2 )}
 
 
 if __name__ == '__main__':
 
     model = open_model()
-    # beam_results(115)
     user_params()
-    # print(Cs_ULS)
 
     lists = model.get_all_saved_lists()
-    # print(lists)
 
     piles_under_pilecap = lists[4]
-    # print(piles_under_pilecap)
 
     elements_string = piles_under_pilecap.description
     elements = convert_to_2_list(elements_string)
-    # print(elements)
-
 
     Fx, Mres, Cs= get_multiple_beam_results(elements)
-    # results = pd.DataFrame.from_dict(get_multiple_beam_results(elements))
-    # results = pd.DataFrame.from_dict(Rs, orient='index')
-    # user_dict = get_multiple_beam_results(elements)
-    # results = pd.DataFrame.from_dict({(i,j): user_dict[i][j] 
-    #                        for i in user_dict.keys() 
-    #                        for j in user_dict[i].keys()},
-    #                    orient='index')
 
     # results.to_csv('data.csv')
 
-    # results['0Mres'] = (results['0Myy']**2+results['0Mzz']**2)
-    # results['1Mres'] = (results['1Myy']**2+results['1Mzz']**2)
-    # print(Cs)
+    ##################################
+
     plt.scatter(Mres,Fx)
 
-    for i in range(len(Mres)-1):
-        M = Mres[i]
-        F = Fx[i]
-        C = Cs[i] 
-        plt.text(x=M, y=F, s=C)
-    plt.show()
-    # print(Fx)
-    # print(Mres)
+    # for i in range(len(Mres)-1):
+    #     M = Mres[i]
+    #     F = Fx[i]
+    #     # C = Cs[i] 
+    #     plt.text(x=M, y=F, s=C)
 
-    # results = pd.DataFrame.from_dict({'Res_0':R0,'Res_1':R1}, index=elements)
-    # results_analysis(results)
+    plt.show()
+
+
 
     del model
